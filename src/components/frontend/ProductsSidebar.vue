f83e7b264fa58acb976e56ab74a69a175e8dcc4e
cms67 <a24985693@gmail.com>
2024-10-20 04:32:19 +0800

    修改：修改商品分類list樣式

--- src/components/frontend/ProductsSidebar.vue
+++ src/components/frontend/ProductsSidebar.vue
@@ -1,30 +1,22 @@
 <template>
-  <div class="list-group list-group-flush d-none d-lg-block" ref="list"
+  <div ref="list"
     @click="changeCategory" @keydown="changeCategory">
-    <button class="list-group-item list-group-item-action" type="button"
+    <button class="btn btn-sm btn-outline-info m-1 m-md-2" type="button"
       :class="{ active: category === '全部' }">全部
     </button>
-    <button class="list-group-item list-group-item-action" type="button"
+    <button class="btn btn-sm btn-outline-info m-1 m-md-2" type="button"
       :class="{ active: category === '花束' }">花束
     </button>
-    <button class="list-group-item list-group-item-action" type="button"
+    <button class="btn btn-sm btn-outline-info m-1 m-md-2" type="button"
       :class="{ active: category === '花瓶' }">花瓶
     </button>
-    <button class="list-group-item list-group-item-action" type="button"
+    <button class="btn btn-sm btn-outline-info m-1 m-md-2" type="button"
       :class="{ active: category === '捧花' }">捧花
     </button>
-    <button class="list-group-item list-group-item-action" type="button"
+    <button class="btn btn-sm btn-outline-info m-1 m-md-2" type="button"
       :class="{ active: category === '乾燥花' }">乾燥花
     </button>
   </div>
-  <select class="form-select d-block d-lg-none" aria-label="Default select example"
-    @change="changeCategory" v-model="category">
-    <option value="全部" selected>全部</option>
-    <option value="花束">花束</option>
-    <option value="花瓶">花瓶</option>
-    <option value="捧花">捧花</option>
-    <option value="乾燥花">乾燥花</option>
-  </select>
 </template>
 
 <script>
